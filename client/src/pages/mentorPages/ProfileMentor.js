3b92d65e8e2127a77c269661f399cd8035f9e0fa
AntonKiya <49375431+AntonKiya@users.noreply.github.com>
2021-01-21 19:37:25 +0300

    covers of pages students and mentors

---
+++ client/src/pages/mentorPages/ProfileMentor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const ProfileMentor = () => {
+
+
+    return(
+        <div>
+            <h1>ProfileMentor</h1>
+        </div>
+    );
+};
---
+++ client/src/pages/studentPages/MyApplicationStudent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const MyProfileStudent = () => {
+
+
+    return(
+        <div>
+            <h1>MyApplicationStudent</h1>
+        </div>
+    );
+};
