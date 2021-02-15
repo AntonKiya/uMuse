b01f14e11de6e633eb4cc2c631b94277d4613eb4
AntonKiya <49375431+AntonKiya@users.noreply.github.com>
2021-02-15 17:03:01 +0300

    important bug fixes and modifications

--- routes/order-mentor.routes.js
+++ routes/order-mentor.routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middleware/auth.middleware');
 const router = Router();
 
 
+//                                  \/
 // /api/order-mentor/suitable0rders
 router.get('/suitable0rders', authMiddleware,async (req, res) => {
     try {
@@ -26,6 +27,33 @@ router.get('/suitable0rders', authMiddleware,async (req, res) => {
             'AND "mentor"."sexMentor_id" = "order".sex_id AND "order".sex_id = "sex".id_sex AND mentor."ageMentor" >= "order"."ageFrom" ' +
             'AND mentor."ageMentor" <= "order"."ageTo" AND "type".id_type = "order".type_id;', [userId]);
 
+        for(item of orders.rows) {
+
+            const result = await pool.query(
+                'SELECT "invited"' +
+                'FROM "responses"' +
+                'WHERE "responses"."order_id" = $1' +
+                'AND "responses"."mentor_id" = $2;', [item.id_order, userId]);
+
+            if(result.rows[0]) {
+
+                if(result.rows[0].invited === 'true') {
+                    const email = await pool.query(
+                        'SELECT "emailStudent" ' +
+                        'FROM "student", "responses", "order"' +
+                        'WHERE "order"."id_order" = $1' +
+                        'AND "student"."id_student" = "order"."student_id" ' +
+                        'AND "responses"."order_id" = "order"."id_order";', [item.id_order]
+                    );
+
+                    item.email = email.rows[0].emailStudent;
+
+                }
+
+            }
+
+        };
+
         res.json(orders.rows);
 
     }catch (e){
@@ -43,6 +71,8 @@ router.get('/oneOrderM/:idOrder', authMiddleware, async (req, res) => {
             return res.status(403).json({message:'У вас нет прав доступа'});
         }
 
+        const userId = req.user.userId;
+
         const idOrder = req.params.idOrder
 
         const order = await pool.query(
@@ -54,6 +84,31 @@ router.get('/oneOrderM/:idOrder', authMiddleware, async (req, res) => {
             'AND "order".sex_id = "sex".id_sex ' +
             'AND "order".type_id = "type".id_type;',[idOrder]);
 
+        console.log();
+
+            const result = await pool.query(
+                'SELECT "invited"' +
+                'FROM "responses"' +
+                'WHERE "responses"."order_id" = $1' +
+                'AND "responses"."mentor_id" = $2;', [order.rows[0].id_order, userId]);
+
+            if(result.rows[0]) {
+
+                if(result.rows[0].invited === 'true') {
+                    const email = await pool.query(
+                        'SELECT "emailStudent" ' +
+                        'FROM "student", "responses", "order"' +
+                        'WHERE "order"."id_order" = $1' +
+                        'AND "student"."id_student" = "order"."student_id" ' +
+                        'AND "responses"."order_id" = "order"."id_order";', [order.rows[0].id_order]
+                    );
+
+                    order.rows[0].email = email.rows[0].emailStudent;
+
+                }
+
+            }
+
         res.json(order.rows[0]);
 
     }catch (e){
@@ -63,6 +118,7 @@ router.get('/oneOrderM/:idOrder', authMiddleware, async (req, res) => {
 
 
 
+//                              \/
 // /api/order-mentor/responses
 router.get('/responses', authMiddleware, async (req,res) => {
     try {
@@ -76,10 +132,64 @@ router.get('/responses', authMiddleware, async (req,res) => {
         const responses = await pool.query(
             'SELECT "id_response", "order_id", "mentor_id", "direction", "experience", "city", "sex", type, "priceFrom", "priceTo", "ageFrom", "ageTo", suggestions FROM responses, direction, "order", "experience", city, sex, type WHERE "mentor_id" = $1 AND "order".id_order = "responses".order_id AND "direction".id_direction = "order".direction_id AND "experience".id_experience = "order".experience_id AND "city".id_city = "order".city_id AND "sex".id_sex = "order".sex_id AND "type".id_type = "order".type_id;', [userId]);
 
+        for(item of responses.rows) {
+
+
+            const result = await pool.query(
+                'SELECT "invited"' +
+                'FROM "responses"' +
+                'WHERE "responses"."order_id" = $1' +
+                'AND "responses"."mentor_id" = $2;', [item.order_id, userId]);
+
+
+            if(result.rows[0].invited === 'true') {
+
+                const email = await pool.query(
+                    'SELECT "emailStudent" ' +
+                    'FROM "student", "responses", "order"' +
+                    'WHERE "order"."id_order" = $1' +
+                    'AND "student"."id_student" = "order"."student_id" ' +
+                    'AND "responses"."order_id" = "order"."id_order";', [item.order_id]
+                );
+
+                item.email = email.rows[0].emailStudent;
+
+            }
+
+        };
+
         res.json(responses.rows);
 
     }catch (e){
         return res.json('Что-то пошло не так в блоке получение ваших отликов ' + e);
+    }
+});
+
+
+// /api/order-mentor/respond
+router.post('/respond', authMiddleware, async (req, res) => {
+    try {
+
+        if (req.user.role !== 'mentor') {
+            return res.status(401).json('У вас нет прав доступа');
+        }
+
+        const mentorId = req.user.userId;
+
+        const orderId = req.body.orderId;
+
+        const result = await pool.query('SELECT * FROM "responses" WHERE "order_id" = $1 AND "mentor_id" = $2;', [orderId, mentorId]);
+
+        if (result.rows[0]) {
+            return res.status(409).json({message: 'Вы уже откликнулись на данную заявку'});
+        }
+
+        await pool.query('INSERT INTO "responses" ("order_id", "mentor_id") VALUES ($1, $2);', [orderId, mentorId]);
+        
+        res.status(201).json({message: 'Вы успешно отликнулись на заявку'});
+
+    }catch (e){
+        return res.json('Что-то пошло не так в блоке откика на заявку' + e);
     }
 });
 
