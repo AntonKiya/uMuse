d79b354a6f9528e73d1d4cb2b4dd2d5feb29664e
AntonKiya <49375431+AntonKiya@users.noreply.github.com>
2021-03-16 23:17:33 +0300

    output photo in profile student

--- client/src/components/studentComponents/ProfileDataS.js
+++ client/src/components/studentComponents/ProfileDataS.js
@@ -6,6 +6,7 @@ export const ProfileDataS = ({dataProfile}) => {
 
     return(
         <div>
+            <img src={`http://localhost:5000/api/user/getPhoto/${dataProfile.photo}`}/>
             <h5 style={{'color':'#ffa000', 'fontWeight': 'bold'}}>id: <span style={{'color':'#03a9f4'}}>{dataProfile.id_student}</span></h5>
             <h5 style={{'color':'#ffa000', 'fontWeight': 'bold'}}>Имя: <span style={{'color':'#03a9f4'}}>{dataProfile.nameStudent}</span></h5>
             <h5 style={{'color':'#ffa000', 'fontWeight': 'bold'}}>Email: <span style={{'color':'#03a9f4'}}>{dataProfile.emailStudent}</span></h5>
